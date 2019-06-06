9b9ef4166d2d74dac267573f8dcd2a6f0ab03c58
Meagan-M <meaganmakinson80@gmail.com>
2019-06-06 09:41:43 -0400

    start word frequency project

--- word_frequency.py
+++ word_frequency.py
@@ -8,6 +8,31 @@ STOP_WORDS = [
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     pass
+    # 1) use open to read in a text file (store the read file in a variable)
+
+    with open(file) as my_file:
+        my_file = my_file.read()
+
+        print(my_file)
+ # -remove punctuation
+#  def clean_text(text):
+    # """
+    # Given a text, return the text with no spaces or punctuation and all lowercased.
+    # """
+    # new_text = ""
+    # text = text.lower()
+    # for character in text:
+    #     if character.isalpha():p
+    #         new_text = new_text + character
+    # return new_text
+
+
+    # 2) calculate the frequency of words
+       
+
+        # -all words are lowercase
+        # -remove stop words
+        # -keep a count of how often a word is used (use a dictionary)
 
 
 if __name__ == "__main__":
