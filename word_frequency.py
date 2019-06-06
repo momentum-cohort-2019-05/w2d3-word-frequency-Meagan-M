3932f5ee68ce1a8cf0afc4f2a4a668d9a750d416
Meagan-M <meaganmakinson80@gmail.com>
2019-06-06 17:55:40 -0400

    split the text successfully

--- word_frequency.py
+++ word_frequency.py
@@ -1,9 +1,10 @@
+import string 
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
-
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
@@ -11,20 +12,27 @@ def print_word_freq(file):
     # 1) use open to read in a text file (store the read file in a variable)
 
     with open(file) as my_file:
-        my_file = my_file.read()
-
-        print(my_file)
+        string_of_file = my_file.read()
+    cleaned_text = clean_text(string_of_file)
+    # print(cleaned_text)
  # -remove punctuation
-#  def clean_text(text):
-    # """
-    # Given a text, return the text with no spaces or punctuation and all lowercased.
-    # """
-    # new_text = ""
-    # text = text.lower()
-    # for character in text:
-    #     if character.isalpha():p
-    #         new_text = new_text + character
-    # return new_text
+
+def clean_text(text):
+    """
+    Given a text, return the text with no spaces or punctuation and all lowercased.
+    """
+    new_text = ""
+    text = text.lower()
+    punctuation = string.punctuation
+    for character in text:
+        if character not in punctuation:
+            new_text += character
+            word_list = new_text.split ()
+        # print(word_list)
+        
+    return new_text
+
+
 
 
     # 2) calculate the frequency of words
